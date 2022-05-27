423d028420fe8d9a9da93483deb739ab6f90250c
Re-taro <aminokacho@gmail.com>
2022-05-27 21:16:39 +0900

    🎉 init

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => ({
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+});
